9af6d05646be656a90daa18b69148bec157af94d
Ashraful Islam <rx.ju.edu@gmail.com>
2024-02-26 21:20:16 +0700

    Update config for tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../../services/mongo");
